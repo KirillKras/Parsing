4a421931121b38924ba712a4c1c60a60b75f2b98
KirillKras <kirill.kras@gmail.com>
2019-12-20 12:35:57 +0300

    ДЗ2: реализован абстрактный класс, базовый и класс VacancyParserHH,      недочет в to_dataframe - защищенный атрибут

--- Lesson_2_OOP_ver.py
+++ Lesson_2_OOP_ver.py
@@ -1,5 +1,6 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABC, abstractmethod
 import re
+import random
 import datetime
 import pandas as pd
 import requests
@@ -11,32 +12,34 @@ from pprint import pprint
 def create_currency_dict():
     currency_dict = {}
     today = datetime.datetime.now().date()
+    url = 'http://www.cbr.ru/scripts/XML_daily.asp'
     params = {'data_req': f'{today.day}/{today.month}/{today.year}'}
-    response = requests.get('http://www.cbr.ru/scripts/XML_daily.asp', params=params)
+    response = requests.get(url, params=params)
     bs_xml = BeautifulSoup(response.text, 'xml')
-    for valute in bs_xml.find_all('Valute'):
-        currency_dict[valute.find('CharCode').text] = float(valute.Value.text.replace(',', '.'))
+    for currency in bs_xml.find_all('Valute'):
+        currency_dict[currency.find('CharCode').text] = float(currency.Value.text.replace(',', '.'))
     currency_dict['RUB'] = 1
     return currency_dict
 
 
-class VacancyParserMeta(metaclass=ABCMeta):
+class VacancyParserABC(ABC):
+
+    @property
+    @abstractmethod
+    def vacancy_list(self):
+        """Read-only свойство - список собранных вакансий"""
+
+    @property
+    @abstractmethod
+    def user_agent_dict(self):
+        """Словарь User-Agent"""
 
     @abstractmethod
-    def get_page(self):
-        pass
-
-    @abstractmethod
-    def get_vacancies(self):
-        pass
+    def to_dataframe(self):
+        """Гарантирует конвертацию результата в pandas.DataFrame"""
 
 
-class VacancyParserBase(VacancyParserMeta):
-    def get_page(self):
-        pass
-
-    def get_vacancies(self):
-        pass
+class VacancyParserBase(VacancyParserABC):
 
     @classmethod
     def convert_compensation(cls, compensation):
@@ -51,7 +54,7 @@ class VacancyParserBase(VacancyParserMeta):
                 currency = 'USD'
             elif 'EUR' in compensation:
                 currency = 'EUR'
-            elif 'грн.' in compensation:
+            elif 'грн' in compensation:
                 currency = 'UAH'
             compensation = compensation.replace(' ', '')
             compensation = compensation.replace(u'\xa0', '')
@@ -66,50 +69,124 @@ class VacancyParserBase(VacancyParserMeta):
                 currency = 'RUB'
         return [min_compensation, max_compensation, currency]
 
+    def __init__(self, search_string,
+                 user_agent_header):
+        self.search_string = search_string
+        self.user_agent_header = self.user_agent_dict[user_agent_header]
+        self._vacancy_list = None
+
+    @property
+    def user_agent_dict(self):
+        return {'chrome': {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) '
+                                         'AppleWebKit/537.36 (KHTML, like Gecko) '},
+
+                'safari': {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) '
+                                         'AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.4 Safari/605.1.15'}
+                }
+
+    @property
+    def vacancy_list(self):
+        return self._vacancy_list
+
+    def to_dataframe(self, currency_dict=None, save_csv=True):
+
+        if self.vacancy_list:
+
+            def convert_currency(row):
+                if row.compensation_min:
+                    row.compensation_min = row.compensation_min * currency_dict[row.compensation_currency]
+                if row.compensation_max:
+                    row.compensation_max = row.compensation_max * currency_dict[row.compensation_currency]
+                return row
+
+            df = pd.DataFrame(self.vacancy_list)
+            df.compensation_min = df.compensation_min.astype(float)
+            df.compensation_max = df.compensation_max.astype(float)
+            df.fillna('', inplace=True)
+            if currency_dict:
+                df = df.apply(convert_currency, axis=1)
+            if save_csv:
+                now = str(datetime.datetime.now()).split('.')[0]
+                time = str.join('', re.findall(r'(\d+)', now))
+                csv_name = f'{self.__class__.__name__}_vacancies_{self.search_string.replace(" ", "-")}_{time}.csv'
+                df.to_csv(csv_name)
+            return df
+        else:
+            print('Нет вакансий')
+
 
 class VacancyParserHH(VacancyParserBase):
 
-    def __init__(self, search_string,
+    def __init__(self, search_string='Data Scientist',
                  url='https://hh.ru/search/vacancy',
-                 page=0):
-        self.search_string = search_string
+                 user_agent_header='safari',
+                 parse_all=True, timer=random.randint(2, 6)):
+        super().__init__(search_string, user_agent_header)
         self.url = url
-        self.page = page
-        self.headers = {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) '
-                          'Chrome/79.0.3945.88 Safari/537.36'}
+        self.parse_all = parse_all
+        self.timer = timer
+        self.__bs_pages = self.__get_pages()
+        if self.__bs_pages:
+            self._vacancy_list = self.__get_vacancy_bs()
 
-    def get_page(self):
-        params = {
-            'text': self.search_string,
-            'page': self.page,
-        }
-        response = requests.get(self.url, params, headers=self.headers)
-        self.page = BeautifulSoup(response.text, 'lxml')
+    def __get_pages(self):
 
-    def get_vacancies(self):
-        if self.page:
-            vacancy_list = []
-            all_vacancies = self.page.find('div', {'class': 'vacancy-serp'})
-            for elem in all_vacancies.contents:
-                if 'data-qa' in elem.attrs and 'vacancy-serp__vacancy' in elem.attrs['data-qa']:
-                    position = elem.find('a', attrs={'class': 'bloko-link HH-LinkModifier'}).text
-                    company = elem.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-employer'}).text
-                    city = elem.find('span', attrs={'data-qa': 'vacancy-serp__vacancy-address'}).text
-                    compensation = elem.find('div', attrs={'data-qa': 'vacancy-serp__vacancy-compensation'})
-                    compensation_min, compensation_max, compensation_currency = \
-                        VacancyParserBase.convert_compensation(compensation)
-                    vacancy_list.append(
-                        {
-                            'position': position,
-                            'company': company,
-                            'city': city,
-                            'compensation_min': compensation_min,
-                            'compensation_max': compensation_max,
-                            'compensation_currency': compensation_currency
-                        }
-                    )
-            pprint(vacancy_list)
-            return vacancy_list, self.page.find('a', {'data-qa': 'pager-next'}) and True
+        def get_page(search_string, page_number):
+            params = {
+                'text': search_string,
+                'page': page_number,
+                'L_save_area': 'true',
+            }
+            response = requests.get(self.url, params, headers=self.user_agent_header)
+            return BeautifulSoup(response.text, 'lxml')
+
+        page_number = 0
+        bs_list = []
+        if self.parse_all:
+            has_page = True
+            while has_page:
+                bs = get_page(self.search_string, page_number)
+                bs_list.append(bs)
+                has_page = bs.find('a', {'data-qa': 'pager-next'}) and True
+                page_number += 1
         else:
-            print('Сначала необходимо получить страницу')
+            bs = get_page(self.search_string, page_number)
+            bs_list.append(bs)
+        return bs_list
+
+    def __get_vacancy_bs(self):
+
+        vacancy_list_all = []
+
+        for bs in self.__bs_pages:
+
+            vacancy_list_page = []
+            vacancies = bs.find_all('div', {'data-qa': 'vacancy-serp__vacancy'})
+
+            for elem in vacancies:
+                position = elem.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-title'}).text
+                company = elem.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-employer'}).text
+                city = elem.find('span', attrs={'data-qa': 'vacancy-serp__vacancy-address'}).text
+                compensation = elem.find('div', attrs={'data-qa': 'vacancy-serp__vacancy-compensation'})
+                compensation_min, compensation_max, compensation_currency = \
+                    VacancyParserBase.convert_compensation(compensation)
+
+                vacancy_list_page.append(
+                    {
+                        'position': position,
+                        'company': company,
+                        'city': city,
+                        'compensation_min': compensation_min,
+                        'compensation_max': compensation_max,
+                        'compensation_currency': compensation_currency
+                    }
+                )
+            vacancy_list_all.extend(vacancy_list_page)
+            time.sleep(self.timer)
+        return vacancy_list_all
+
+
+if __name__ == '__main__':
+    currency_dict = create_currency_dict()
+    hh_parser = VacancyParserHH()
+    hh_parser.to_dataframe(currency_dict=currency_dict)
