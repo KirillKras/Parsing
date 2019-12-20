8c73bf1c1c3cc2e4f4836b8d6d3f7ae52ef98ba8
KirillKras <kirill.kras@gmail.com>
2019-12-20 14:45:43 +0300

    ДЗ2: добавлены классы фильтры тегов (namedtuple)

--- Lesson_2_OOP_ver.py
+++ Lesson_2_OOP_ver.py
@@ -2,13 +2,15 @@ from abc import ABC, abstractmethod
 import re
 import random
 import datetime
+import time
+from collections import namedtuple
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-import time
 from pprint import pprint
 
 
+# Функция получения курсов валют от ЦБ
 def create_currency_dict():
     currency_dict = {}
     today = datetime.datetime.now().date()
@@ -17,11 +19,17 @@ def create_currency_dict():
     response = requests.get(url, params=params)
     bs_xml = BeautifulSoup(response.text, 'xml')
     for currency in bs_xml.find_all('Valute'):
-        currency_dict[currency.find('CharCode').text] = float(currency.Value.text.replace(',', '.'))
+        currency_dict[currency.find('CharCode').text] = float(currency.Value.text.replace(',', '.')) / float(currency.Nominal.text)
     currency_dict['RUB'] = 1
     return currency_dict
 
 
+# Классы фильтров интересующих данных
+TagFilter = namedtuple('TagFilter', 'tag attrs')
+Filter = namedtuple('Filter', 'vacancy position company city compensation')
+
+
+# Абстрактный класс парсеров, декларирующий обязательный интерфейс
 class VacancyParserABC(ABC):
 
     @property
@@ -39,6 +47,7 @@ class VacancyParserABC(ABC):
         """Гарантирует конвертацию результата в pandas.DataFrame"""
 
 
+# Базовый класс парсеров, реализующий словарь браузеров, функцию конвертации в pandas.DataFrame
 class VacancyParserBase(VacancyParserABC):
 
     @classmethod
@@ -56,6 +65,8 @@ class VacancyParserBase(VacancyParserABC):
                 currency = 'EUR'
             elif 'грн' in compensation:
                 currency = 'UAH'
+            else:
+                currency = 'RUB'
             compensation = compensation.replace(' ', '')
             compensation = compensation.replace(u'\xa0', '')
             pattern = re.compile(r'(\d+)')
@@ -63,17 +74,18 @@ class VacancyParserBase(VacancyParserABC):
             if len(min_max) == 2:
                 min_compensation = min_max[0]
                 max_compensation = min_max[1]
-                currency = 'RUB'
             elif len(min_max) == 1:
                 min_compensation = min_max[0]
-                currency = 'RUB'
         return [min_compensation, max_compensation, currency]
 
     def __init__(self, search_string,
                  user_agent_header):
         self.search_string = search_string
         self.user_agent_header = self.user_agent_dict[user_agent_header]
+        self.filter = None
+        self.timer = None
         self._vacancy_list = None
+        self._bs_pages = None
 
     @property
     def user_agent_dict(self):
@@ -88,15 +100,48 @@ class VacancyParserBase(VacancyParserABC):
     def vacancy_list(self):
         return self._vacancy_list
 
-    def to_dataframe(self, currency_dict=None, save_csv=True):
+    def _get_vacancy_bs(self):
+        vacancy_list_all = []
+        for bs in self._bs_pages:
+            vacancy_list_page = []
+            vacancies = bs.find_all(self.filter.vacancy.tag, self.filter.vacancy.attrs)
+            for elem in vacancies:
+                position = elem.find(self.filter.position.tag, self.filter.position.attrs).text
+                company = elem.find(self.filter.company.tag, self.filter.company.attrs)
+                company = company.text if company else company
+                city = elem.find(self.filter.city.tag, self.filter.city.attrs).text
+                city = city.split(' • ')[1] if ' • ' in city else city
+                compensation = elem.find(self.filter.compensation.tag, self.filter.compensation.attrs)
+                compensation_min, compensation_max, compensation_currency = \
+                    VacancyParserBase.convert_compensation(compensation)
+                vacancy_list_page.append(
+                    {
+                        'position': position,
+                        'company': company,
+                        'city': city,
+                        'compensation_min': compensation_min,
+                        'compensation_max': compensation_max,
+                        'compensation_currency': compensation_currency
+                    }
+                )
+            pprint(vacancy_list_page)
+            vacancy_list_all.extend(vacancy_list_page)
+            time.sleep(self.timer)
+        return vacancy_list_all
 
+    def to_dataframe(self, currency_dict=None, save_csv=True):
         if self.vacancy_list:
 
             def convert_currency(row):
+                convert_flag = False
                 if row.compensation_min:
                     row.compensation_min = row.compensation_min * currency_dict[row.compensation_currency]
+                    convert_flag = True
                 if row.compensation_max:
                     row.compensation_max = row.compensation_max * currency_dict[row.compensation_currency]
+                    convert_flag = True
+                if convert_flag:
+                    row.compensation_currency = 'RUB'
                 return row
 
             df = pd.DataFrame(self.vacancy_list)
@@ -115,6 +160,7 @@ class VacancyParserBase(VacancyParserABC):
             print('Нет вакансий')
 
 
+# Класс, реализующий парсер вакансий в HH
 class VacancyParserHH(VacancyParserBase):
 
     def __init__(self, search_string='Data Scientist',
@@ -125,9 +171,15 @@ class VacancyParserHH(VacancyParserBase):
         self.url = url
         self.parse_all = parse_all
         self.timer = timer
-        self.__bs_pages = self.__get_pages()
-        if self.__bs_pages:
-            self._vacancy_list = self.__get_vacancy_bs()
+        self.filter = Filter(
+            vacancy=TagFilter(tag='div', attrs={'data-qa': 'vacancy-serp__vacancy'}),
+            position=TagFilter(tag='a', attrs={'data-qa': 'vacancy-serp__vacancy-title'}),
+            company=TagFilter(tag='a', attrs={'data-qa': 'vacancy-serp__vacancy-employer'}),
+            city=TagFilter(tag='span', attrs={'data-qa': 'vacancy-serp__vacancy-address'}),
+            compensation=TagFilter(tag='div', attrs={'data-qa': 'vacancy-serp__vacancy-compensation'}))
+        self._bs_pages = self.__get_pages()
+        if self._bs_pages:
+            self._vacancy_list = self._get_vacancy_bs()
 
     def __get_pages(self):
 
@@ -154,38 +206,8 @@ class VacancyParserHH(VacancyParserBase):
             bs_list.append(bs)
         return bs_list
 
-    def __get_vacancy_bs(self):
 
-        vacancy_list_all = []
-
-        for bs in self.__bs_pages:
-
-            vacancy_list_page = []
-            vacancies = bs.find_all('div', {'data-qa': 'vacancy-serp__vacancy'})
-
-            for elem in vacancies:
-                position = elem.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-title'}).text
-                company = elem.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-employer'}).text
-                city = elem.find('span', attrs={'data-qa': 'vacancy-serp__vacancy-address'}).text
-                compensation = elem.find('div', attrs={'data-qa': 'vacancy-serp__vacancy-compensation'})
-                compensation_min, compensation_max, compensation_currency = \
-                    VacancyParserBase.convert_compensation(compensation)
-
-                vacancy_list_page.append(
-                    {
-                        'position': position,
-                        'company': company,
-                        'city': city,
-                        'compensation_min': compensation_min,
-                        'compensation_max': compensation_max,
-                        'compensation_currency': compensation_currency
-                    }
-                )
-            vacancy_list_all.extend(vacancy_list_page)
-            time.sleep(self.timer)
-        return vacancy_list_all
-
-
+# Класс, реализующий парсер вакансий в SJ
 class VacancyParserSJ(VacancyParserBase):
 
     def __init__(self, search_string='Data Scientist',
@@ -196,9 +218,22 @@ class VacancyParserSJ(VacancyParserBase):
         self.url = url
         self.parse_all = parse_all
         self.timer = timer
-        self.__bs_pages = self.__get_pages()
-        if self.__bs_pages:
-            self._vacancy_list = self.__get_vacancy_bs()
+        self.filter = Filter(
+            vacancy=TagFilter(tag='div',
+                              attrs={'class': '_3zucV _2GPIV f-test-vacancy-item i6-sc _3VcZr'}),
+            position=TagFilter(tag='div',
+                               attrs={'class': '_3mfro CuJz5 PlM3e _2JVkc _3LJqf'}),
+            company=TagFilter(tag='span',
+                              attrs={
+                                  'class': '_3mfro _3Fsn4 f-test-text-vacancy-item-company-name _9fXTd _2JVkc _3e53o _15msI'}),
+            city=TagFilter(tag='span',
+                           attrs={'class': '_3mfro f-test-text-company-item-location _9fXTd _2JVkc _3e53o'}),
+            compensation=TagFilter(tag='span',
+                                   attrs={
+                                       'class': '_3mfro _2Wp8I f-test-text-company-item-salary PlM3e _2JVkc _2VHxz'}))
+        self._bs_pages = self.__get_pages()
+        if self._bs_pages:
+            self._vacancy_list = self._get_vacancy_bs()
 
     def __get_pages(self):
 
@@ -215,11 +250,9 @@ class VacancyParserSJ(VacancyParserBase):
                 }
                 response = requests.get(self.url, params=params, headers=self.user_agent_header)
                 return BeautifulSoup(response.text, 'lxml')
-
             return get_next_response
 
         get_page_func = get_page(self.search_string)
-
         page_number = 1
         bs_list = []
         if self.parse_all:
@@ -234,46 +267,8 @@ class VacancyParserSJ(VacancyParserBase):
             bs_list.append(bs)
         return bs_list
 
-    def __get_vacancy_bs(self):
-
-        vacancy_list_all = []
-
-        for bs in self.__bs_pages:
-
-            vacancy_list_page = []
-            vacancies = bs.find_all('div', {'class': '_3zucV _2GPIV f-test-vacancy-item i6-sc _3VcZr'})
-
-            for elem in vacancies:
-                position = elem.find('div', {'class': '_3mfro CuJz5 PlM3e _2JVkc _3LJqf'}).text
-                company = elem.find('span',
-                                    {
-                                        'class': '_3mfro _3Fsn4 f-test-text-vacancy-item-company-name _9fXTd _2JVkc _3e53o _15msI'})
-                if company:
-                    company = company.text
-                city = elem.find('span',
-                                 {'class': '_3mfro f-test-text-company-item-location _9fXTd _2JVkc _3e53o'}).text.split(
-                    ' • ')[1]
-                compensation = elem.find('span',
-                                         {'class': '_3mfro _2Wp8I f-test-text-company-item-salary PlM3e _2JVkc _2VHxz'})
-                compensation_min, compensation_max, compensation_currency = \
-                    VacancyParserBase.convert_compensation(compensation)
-                vacancy_list_page.append(
-                    {
-                        'position': position,
-                        'company': company,
-                        'city': city,
-                        'compensation_min': compensation_min,
-                        'compensation_max': compensation_max,
-                        'compensation_currency': compensation_currency
-                    }
-                )
-            pprint(vacancy_list_page)
-            vacancy_list_all.extend(vacancy_list_page)
-            time.sleep(self.timer)
-        return vacancy_list_all
-
 
 if __name__ == '__main__':
     currency_dict = create_currency_dict()
-    parser = VacancyParserSJ(search_string='аналитик')
+    parser = VacancyParserHH(search_string='Data scientist', user_agent_header='chrome')
     parser.to_dataframe(currency_dict=currency_dict)
