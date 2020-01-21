1eba155385211b20e2a1d31ecc54b8753a429ced
KirillKras <kirill.kras@gmail.com>
2020-01-21 11:20:22 +0300

    ДЗ6: реализация avitoparser по примеру урока

--- avitoparser/items.py
+++ avitoparser/items.py
@@ -6,9 +6,15 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader.processors import MapCompose, TakeFirst
 
+
+def cleaner_photo(img):
+    if img[:2] == '//':
+        img = f'http:{img}'
+    return img
 
 class AvitoparserItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    _id = scrapy.Field()
+    name = scrapy.Field(output_processor=TakeFirst())
+    photos = scrapy.Field(input_processor=MapCompose(cleaner_photo))
--- avitoparser/settings.py
+++ avitoparser/settings.py
@@ -14,23 +14,31 @@ BOT_NAME = 'avitoparser'
 SPIDER_MODULES = ['avitoparser.spiders']
 NEWSPIDER_MODULE = 'avitoparser.spiders'
 
+LOG_ENABLED = True
+LOG_LEVEL = 'DEBUG'
+
+IMAGES_STORE = 'images'
+
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) ' \
+             'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
+
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'avitoparser (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 6
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 8
+CONCURRENT_REQUESTS_PER_IP = 8
 
 # Disable cookies (enabled by default)
 #COOKIES_ENABLED = False
@@ -64,9 +72,10 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'avitoparser.pipelines.AvitoparserPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'avitoparser.pipelines.AvitoPhotosPipeline': 200,
+   'avitoparser.pipelines.DatabasePipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
